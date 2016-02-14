5c1fb02a1a708a29722d25056c8c3a8630a1c73c
Ryan's iMac <ryancmoret@gmail.com>
2016-02-14 16:46:18 -0600

    Added spec/features, including balance feature

---
+++ spec/features/account_balance_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe 'account.balance' do
+  let(:primary_account) { FactoryGirl.create(:account) }
+  context 'account without transactions' do
+    it 'should return 0' do
+      expect(primary_account.transactions.count).to be 0
+      expect(primary_account.balance).to eq 0
+    end
+  end
+  context 'account with primary transactions' do
+  end
+  context 'accout with primary and sub-transactions' do
+  end
+  context 'multiple accounts' do
+  end
+end
