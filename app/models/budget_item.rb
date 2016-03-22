b6cadb643927499e8886480d139a2128f608d08e
rmoret <rmoret@covermymeds.com>
2016-03-22 12:53:39 -0400

    Added monthly/weekly & expenses/revenues scopes to budget item

--- app/models/budget_item.rb
+++ app/models/budget_item.rb
@@ -1,4 +1,9 @@
 class BudgetItem < ActiveRecord::Base
   has_many :budgeted_amounts
   has_many :transactions, through: :budgeted_amounts
+
+  scope :monthly,  -> { where(monthly: true) }
+  scope :weekly,   -> { where(monthly: false) }
+  scope :expenses, -> { where(expense: true) }
+  scope :revenues, -> { where(expense: false) }
 end
