aa6807c84e403005378fd052d9509a33a320d895
rmoret <rmoret@covermymeds.com>
2016-03-25 09:14:10 -0400

    Cleaned up the spec helper file a bit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,27 +7,22 @@ require './spec/helpers/custom_matchers'
 Rake::Task['app:setup'].invoke
 
 RSpec.configure do |config|
+  config.include(Helpers::CustomMatchers)
   config.include(Rack::Test::Methods)
   config.include(Shoulda::Matchers::ActiveModel, type: :model)
   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
   config.include(Shoulda::Matchers::Independent)
-  config.include(Helpers::CustomMatchers)
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
 
-  config.example_status_persistence_file_path = "spec/examples.txt"
   config.disable_monkey_patching!
 
   if config.files_to_run.one?
     config.default_formatter = 'doc'
   end
 
-  # config.profile_examples = 10
-
-  # Kernel.srand config.seed
-  # ActiveRecord::Base.connection
   FactoryGirl.definition_file_paths = %w{./spec/factories}
   FactoryGirl.find_definitions
   config.before(:suite) do
