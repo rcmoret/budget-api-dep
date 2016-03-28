218dcf9dbb1a783e364967f2896a1d7f5d9a62d4
rmoret <rmoret@covermymeds.com>
2016-03-28 17:18:27 -0500

    Edited view SQL file

--- db/migrate/20160205172413_create_transactions_view.rb
+++ db/migrate/20160205172413_create_transactions_view.rb
@@ -1,21 +1,36 @@
 class CreateTransactionsView < ActiveRecord::Migration
+  SUM_QRY = %Q{COALESCE((SELECT sum(amount) FROM transactions WHERE primary_transaction_id = t.id), t.amount)}
+
+  SUB_QRY = <<-SQL
+    TO_JSON(ARRAY(
+      SELECT JSON_BUILD_OBJECT('id', sub.id, 'budget_item', i.name, 'description', sub.description, 'amount', sub.amount)
+      FROM transactions sub
+      LEFT OUTER JOIN monthly_amounts m1 ON m1.id = sub.monthly_amount_id
+      LEFT JOIN budget_items i ON i.id = m1.budget_item_id
+      WHERE sub.primary_transaction_id = t.id
+    ))
+  SQL
+
   def up
     execute <<-SQL
       CREATE VIEW transaction_view AS
-        SELECT t.id, t.description AS "description", b.name, t.clearance_date AS "clearance_date", t.notes, t.receipt, t.check_number, t.account_id AS "account_id",
-          COALESCE((SELECT sum(amount) FROM transactions WHERE primary_transaction_id = t.id), t.amount) AS amount,
-          TO_JSON(ARRAY(
-            SELECT JSON_BUILD_OBJECT('id', sub.id, 'name', i.name, 'description', sub.description, 'amount', sub.amount)
-            FROM transactions sub
-            LEFT OUTER JOIN monthly_amounts m1 ON m1.id = sub.monthly_amount_id
-            LEFT JOIN budget_items i ON i.id = m1.budget_item_id
-            WHERE sub.primary_transaction_id = t.id
-          )) AS subtransactions
+        SELECT t.id, t.description AS "description",
+               b.name AS "budget_item",
+               t.clearance_date AS "clearance_date",
+               #{SUM_QRY} AS amount,
+               a.name AS "account_name",
+               a.id AS "account_id",
+               t.check_number,
+               t.receipt,
+               t.notes,
+               t.tax_deduction,
+               t.qualified_medical_expense,
+               #{SUB_QRY}  AS subtransactions
         FROM transactions t
         LEFT OUTER JOIN monthly_amounts ma ON ma.id = t.monthly_amount_id
         LEFT JOIN budget_items b on b.id = ma.budget_item_id
+        LEFT JOIN accounts a ON a.id = t.account_id
         WHERE t.primary_transaction_id IS NULL
-
     SQL
   end
 
