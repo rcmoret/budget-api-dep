6b7afcb32b16959e701af39ac513ae4b775296d7
Ryan's iMac <ryancmoret@gmail.com>
2016-03-30 23:10:03 -0500

    Added spec for PUT(update) route for transactions

--- spec/requests/account_transaction_requests_spec.rb
+++ spec/requests/account_transaction_requests_spec.rb
@@ -33,17 +33,48 @@ RSpec.describe 'AccountsApi /transactions/:id', type: :request do
       include_examples 'JSON transaction with subtransactions'
     end
   end
-  describe 'transactions POST' do
-    let(:attrs) { { account: checking, description: 'Kroger', amount: -20.5 } }
-    let(:update_attributes) { { clearance_date: '2015-03-14' } }
-    let(:transaction_attributes) { attrs.merge(update_attributes) }
+  describe 'transactions PUT' do
     let(:transaction) { FactoryGirl.create(:transaction, attrs) }
     let(:endpoint) { super() + "/#{transaction.id}" }
+    let(:response) { put endpoint, update_attributes }
+    subject { JSON.parse(response.body) }
     context 'simple transaction' do
-      let(:response) { put endpoint, update_attributes }
-      subject { JSON.parse(response.body) }
+      let(:attrs) { { account: checking, description: 'Kroger', amount: -20.5 } }
+      let(:transaction_attributes) { attrs.merge(update_attributes) }
+      let(:update_attributes) { { clearance_date: '2015-03-14' } }
       it { expect(response.status).to be 200 }
       include_examples 'JSON transaction'
+    end
+    context 'transaction with subs' do
+      let(:attrs) do
+        { account: checking,
+          description: 'Costco',
+          amount: nil,
+          subtransactions_attributes: original_sub_attributes }
+      end
+      let(:original_sub_attributes) do
+        [ { description: 'Clothes', amount: -20.0 },
+          { description: 'Food', amount: -35.0 } ]
+      end
+      let(:update_attributes) do
+        { subtransactions_attributes:
+          [
+            { id: transaction.subtransactions.first.id, amount: -14.0 },
+            { id: transaction.subtransactions.last.id, amount: -22.45 }
+          ]
+        }
+      end
+      let(:transaction_attributes) { attrs.deep_merge(update_attributes) }
+      let(:amount) do
+        update_attributes[:subtransactions_attributes].inject(0) do |sum, sub|
+          sum += sub[:amount]
+        end
+      end
+      it { expect(response.status).to be 200 }
+      it 'should change the subtransactions' do
+        response
+        expect(transaction.reload.view.amount).to eq amount
+      end
     end
   end
 end
