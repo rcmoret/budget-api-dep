9b36e868262d05ca42afa8b9e433e94a2c7cd507
rmoret <rmoret@covermymeds.com>
2016-04-01 12:35:57 -0500

    Moved BudgetItemApi methods into a module

--- app/api/budget_item_api.rb
+++ app/api/budget_item_api.rb
@@ -1,40 +1,19 @@
 class BudgetItemApi < Sinatra::Base
   include SharedHelpers
+  include Helpers::BudgetItemApiHelpers
 
   get '/' do
     BudgetItem.all.map(&:to_hash).to_json
   end
 
   post '/' do
-    if budget_item.save
-      render_new(budget_item)
-    else
-      render_error(400)
-    end
+    budget_item.save ? render_new(budget_item) : render_error(400)
   end
 
   get '/:id' do
     budget_item.to_json
   end
 
-  def item_id
-    params['id']
-  end
-
-  def budget_item
-    @item ||= find_or_create_item!
-  end
-
-  def find_or_create_item!
-    if item_id.present?
-      BudgetItem.find_by_id(item_id) || render_404('budget_item', item_id)
-    else
-      BudgetItem.new(create_params)
-    end
-  end
-
-  def create_params
-    require_parameters!('name', 'default_amount')
-    params.slice(*BudgetItem::PUBLIC_ATTRS)
-  end
+  # put '/:id' do
+  # end
 end
--- app/helpers/api_helpers.rb
+++ app/helpers/api_helpers.rb
@@ -39,4 +39,27 @@ module Helpers
       transaction
     end
   end
+
+  module BudgetItemApiHelpers
+    def item_id
+      params['id']
+    end
+
+    def budget_item
+      @item ||= find_or_create_item!
+    end
+
+    def find_or_create_item!
+      if item_id.present?
+        BudgetItem.find_by_id(item_id) || render_404('budget_item', item_id)
+      else
+        BudgetItem.new(create_params)
+      end
+    end
+
+    def create_params
+      require_parameters!('name', 'default_amount')
+      params.slice(*BudgetItem::PUBLIC_ATTRS)
+    end
+  end
 end
