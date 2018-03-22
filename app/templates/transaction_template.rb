b815f95fc3591a9b9faa06c21263cd392894c068
Ryan's iMac <ryancmoret@gmail.com>
2018-03-22 11:41:38 -0500

    Cleanup in transaction template

--- app/templates/transaction_template.rb
+++ app/templates/transaction_template.rb
@@ -1,42 +1,36 @@
 class TransactionTemplate
+  attr_reader :account, :options
   def initialize(account, **options)
     @account = account
     @options = options
-    set_date_range!
   end
 
   def metadata
-    @metadata ||= collect_metadata!
+    @metadata ||= {
+      date_range: [date_range.first, date_range.last],
+      prior_balance: account.transactions.prior_to(date_range.first).total,
+      query_options: options,
+    }
   end
 
   def collection
-    @collection ||= collect_transactions!
+    @collection ||= account.transactions.between(
+      date_range, include_pending: options[:include_pending]
+    ).as_collection
   end
 
   private
 
-  def set_date_range!
-    @date_range = case
-                  when @options[:date]
-                    BudgetMonth.new(@options[:date]).date_range
-                  when @options[:month]
-                    BudgetMonth.new(@options).date_range
-                  when @options[:first] && @options[:last]
-                    (options[:first]..options[:last])
-                  else
-                    BudgetMonth.new.date_range
-                  end
-  end
-
-  def collect_metadata!
-    {
-      date_range: [@date_range.first, @date_range.last],
-      prior_balance: @account.transactions.prior_to(@date_range.first).total,
-      query_options: @options.except(:first, :last, :month, :date)
-    }
-  end
-
-  def collect_transactions!
-    @account.transactions.between(@date_range, include_pending: true).as_collection
+  def date_range
+    @date_range ||= case
+                    when options[:date]
+                      BudgetMonth.new(options[:date]).date_range
+                    when options[:month]
+                      BudgetMonth.new(options).date_range
+                    when options[:first] && options[:last]
+                      (options[:first]..options[:last])
+                    else
+                      BudgetMonth.new.date_range
+                    end
   end
 end
