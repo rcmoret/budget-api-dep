ce7fe9b773d0e345da7da3b46a09fb355cd1dc78
Ryan's iMac <ryancmoret@gmail.com>
2019-02-07 16:40:49 -0600

    Simplify discretionary class

--- lib/discretionary.rb
+++ lib/discretionary.rb
@@ -5,19 +5,13 @@ class Discretionary
     @budget_month = budget_month
   end
 
-  delegate :current?, :date_hash, :date_range, :days_remaining,
-                      :first_day, :status, :total_days, to: :budget_month
+  delegate :current?, :date_range, :days_remaining, :total_days, to: :budget_month
 
   def to_hash
     {
       name: 'Discretionary',
       spent: spent,
-      over_under_budget: over_under_budget,
-      remaining: {
-        remaining_budgeted: remaining_budgeted,
-        available_cash: available_cash,
-        charged: charged,
-      },
+      balance: balance,
       expense: true,
       days_remaining: days_remaining,
       total_days: total_days,
@@ -33,27 +27,11 @@ class Discretionary
 
   private
 
-  def available_cash
-    @available_cash ||= Account.available_cash.to_i
-  end
-
-  def beginning_balance
-    @beginning_balance ||= Account.balance_prior_to(first_day).to_i
-  end
-
-  def remaining_budgeted
-    @remaining_budgeted ||= Budget::Item.remaining_for(date_hash).to_i
-  end
-
-  def charged
-    @charged ||= Account.charged.to_i
+  def balance
+    @balance ||= Account.available_cash.to_i + Account.charged.to_i
   end
 
   def spent
-    @spent ||= transactions.total.to_i
-  end
-
-  def over_under_budget
-    @over_under_budget ||= Budget::Item.over_under_budget(date_hash).to_i
+    transactions.total.to_i
   end
 end
--- spec/lib/discretionary_spec.rb
+++ spec/lib/discretionary_spec.rb
@@ -2,11 +2,9 @@ require 'spec_helper'
 
 RSpec.describe Discretionary do
   context 'current month' do
-    let(:items_remaining) { (-1000..1000).to_a.sample }
     let(:available_cash) { (3000..3400).to_a.sample }
     let(:charged) { (-1000..100).to_a.sample }
     before do
-      allow(Budget::Item).to receive(:remaining_for) { items_remaining }
       allow(Account).to receive(:available_cash) { available_cash }
       allow(Account).to receive(:charged) { charged }
     end
@@ -18,28 +16,10 @@ RSpec.describe Discretionary do
       it { expect(subject).to eq 'Discretionary' }
     end
 
-    describe '[:remaining][:remaining_budgeted]' do
-      subject { discretionary[:remaining][:remaining_budgeted] }
-      it { should be items_remaining }
-    end
+    describe '[:balance]' do
+      subject { discretionary[:balance] }
 
-    describe '[:remaining][:available_cash]' do
-      subject { discretionary[:remaining][:available_cash] }
-      it { should be available_cash }
-    end
-
-    describe '[:remaining][:charged]' do
-      subject { discretionary[:remaining][:charged] }
-      it { should be charged }
-    end
-
-    describe '[:over_under_budget]' do
-      let(:amount) { (-1000..1000).to_a.sample }
-      before { allow(Budget::Item).to receive(:over_under_budget) { amount } }
-
-      subject { discretionary[:over_under_budget] }
-
-      it { expect(subject).to eq amount }
+      it { expect(subject).to eq (available_cash + charged) }
     end
 
     describe '[:days_remaining]' do
