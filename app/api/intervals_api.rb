c27312dac6d25b5f2cf4bfbb29af0d5331b59724
Ryan's iMac <ryancmoret@gmail.com>
2019-05-14 21:47:53 -0500

    Make url params a little more strict for intervals api endpoint

--- app/api/intervals_api.rb
+++ app/api/intervals_api.rb
@@ -2,7 +2,7 @@ class IntervalsApi < Sinatra::Base
   register Sinatra::Namespace
   include SharedHelpers
 
-  put %r{/(?<month>\d+)/(?<year>\d+)} do
+  put %r{/(?<month>\d{1,2})/(?<year>\d{4})} do
     interval = Budget::Interval.for(sym_params)
     interval.update(sym_params.slice(*Budget::Interval::PUBLIC_ATTRS))
     render_updated(interval)
