bd9bd95d6df41e7ecff2d6ea468b97fef043667e
Rusty Moret <rmoret@covermymeds.com>
2020-03-07 15:26:10 -0500

    updates to schema file

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20200131220723) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "accounts", force: :cascade do |t|
     t.string "name", null: false
     t.boolean "cash_flow", default: true
@@ -31,7 +28,7 @@ ActiveRecord::Schema.define(version: 20200131220723) do
     t.boolean "expense", default: true, null: false
     t.boolean "accrual", default: false, null: false
     t.datetime "archived_at"
-    t.bigint "icon_id"
+    t.integer "icon_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["icon_id"], name: "index_budget_categories_on_icon_id"
@@ -56,8 +53,8 @@ ActiveRecord::Schema.define(version: 20200131220723) do
     t.integer "month"
     t.integer "year"
     t.integer "amount"
-    t.bigint "budget_category_id", null: false
-    t.bigint "budget_interval_id", null: false
+    t.integer "budget_category_id", null: false
+    t.integer "budget_interval_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["budget_category_id"], name: "index_budget_items_on_budget_category_id"
@@ -72,8 +69,8 @@ ActiveRecord::Schema.define(version: 20200131220723) do
   end
 
   create_table "transaction_details", force: :cascade do |t|
-    t.bigint "transaction_entry_id", null: false
-    t.bigint "budget_item_id"
+    t.integer "transaction_entry_id", null: false
+    t.integer "budget_item_id"
     t.integer "amount", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -85,10 +82,10 @@ ActiveRecord::Schema.define(version: 20200131220723) do
     t.string "description", limit: 255
     t.string "check_number", limit: 12
     t.date "clearance_date"
-    t.bigint "account_id", null: false
+    t.integer "account_id", null: false
     t.text "notes"
     t.boolean "budget_exclusion", default: false, null: false
-    t.bigint "transfer_id"
+    t.integer "transfer_id"
     t.string "receipt", limit: 255
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -103,13 +100,4 @@ ActiveRecord::Schema.define(version: 20200131220723) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "budget_categories", "icons"
-  add_foreign_key "budget_category_maturity_intervals", "budget_categories"
-  add_foreign_key "budget_category_maturity_intervals", "budget_intervals"
-  add_foreign_key "budget_items", "budget_categories"
-  add_foreign_key "budget_items", "budget_intervals"
-  add_foreign_key "transaction_details", "budget_items"
-  add_foreign_key "transaction_details", "transaction_entries"
-  add_foreign_key "transaction_entries", "accounts"
-  add_foreign_key "transaction_entries", "transfers"
 end
