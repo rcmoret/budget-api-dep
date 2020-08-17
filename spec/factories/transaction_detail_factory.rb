6cac56e58349499dc447324de6d2c02e7af1ae4d
Ryan's iMac <ryancmoret@gmail.com>
2020-08-17 11:26:58 -0500

    Hopefully fix some flaky tests where amount was 0 and item was nil

--- spec/factories/transaction_detail_factory.rb
+++ spec/factories/transaction_detail_factory.rb
@@ -4,6 +4,8 @@ FactoryBot.define do
   factory :transaction_detail, class: Transaction::Detail do
     association :budget_item
     association :entry, factory: :transaction_entry
-    amount { (-10_000..10_000).to_a.sample }
+    amount do
+      (-10_000..10_000).to_a.reject(&:zero?).sample
+    end
   end
 end
